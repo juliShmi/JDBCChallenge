0144484c67e2c08e81cc9d109530c3f6ed57ad2b
juliShmi <juliashmitko@gmail.com>
2021-05-18 14:40:17 +0300

    Driver

---
+++ src/main/java/de/telekom/jdbcchallenge/Main.java
@@ -0,0 +1,19 @@
+package de.telekom.jdbcchallenge;
+
+
+
+public class Main {
+	
+	final static String DRIVER = "org.mariadb.jdbc.Driver";
+	
+	
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		Class.forName(DRIVER);
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+
+}
